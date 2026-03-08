dcdac0dbce95652eb017441707e4c084932c7cb7
Pawani541 <2005pawani@gmail.com>
2026-03-08 12:39:03 +0530

    Add files via upload

--- Week1--6_Jan_26to11_Jan_26/Day4_9_Jan_2026/Program.cs
+++ Week1--6_Jan_26to11_Jan_26/Day4_9_Jan_2026/Program.cs
@@ -1,34 +1,26 @@
 ﻿using System;
-class EvenOddAvg
+
+class EvenDigitSum
 {
     static void Main()
     {
-        int size = int.Parse(Console.ReadLine());
+        Console.Write("Enter number: ");
+        int n = int.Parse(Console.ReadLine());
         int output1 = 0;
 
-        if (size < 0) output1 = -2;
+        if (n < 0) output1 = -1;
+        else if (n > 32767) output1 = -2;
         else
         {
-            int[] arr = new int[size];
-            bool neg = false;
-            for (int i = 0; i < size; i++)
+            while (n > 0)
             {
-                arr[i] = int.Parse(Console.ReadLine());
-                if (arr[i] < 0) neg = true;
-            }
-
-            if (neg) output1 = -1;
-            else
-            {
-                int sumEven = 0, sumOdd = 0;
-                foreach (int v in arr)
-                {
-                    if (v % 2 == 0) sumEven += v;
-                    else sumOdd += v;
-                }
-                output1 = (sumEven + sumOdd) / 2;
+                int d = n % 10;
+                if (d % 2 == 0)
+                    output1 += d;
+                n /= 10;
             }
         }
-        Console.WriteLine(output1);
+
+        Console.WriteLine("\nOutput: " + output1);
     }
 }
