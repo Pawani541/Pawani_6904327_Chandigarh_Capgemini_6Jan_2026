9c409cc21c6debc783966a6b4f5558f7d4f0ee4b
Pawani541 <2005pawani@gmail.com>
2026-03-08 13:11:56 +0530

    Add files via upload

--- Week2--12_Jan_26to18_Jan_26/Day4_15_Jan_2026/Program.cs
+++ Week2--12_Jan_26to18_Jan_26/Day4_15_Jan_2026/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 class Program
 {
@@ -10,21 +9,53 @@ class Program
 
         if (n < 0)
         {
-            Console.WriteLine(-2);
-            return;
+            Console.WriteLine(-2); return; 
         }
 
         int[] arr = new int[n];
         Console.WriteLine("Enter elements:");
-        for (int i = 0; i < n; i++) arr[i] = Convert.ToInt32(Console.ReadLine());
-
-        if (arr.Any(x => x < 0))
+        for (int i = 0; i < n; i++)
         {
-            Console.WriteLine(-1);
-            return;
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+            if (arr[i] < 0) { Console.WriteLine(-1); return; }
         }
 
-        int ans = arr.OrderByDescending(x => x).Distinct().Skip(1).First();
-        Console.WriteLine(ans);
+        Console.Write("Enter number to insert: ");
+        int insert = Convert.ToInt32(Console.ReadLine());
+
+        // Sort (Bubble Sort)
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (arr[i] > arr[j])
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        // insert
+        int[] output = new int[n + 1];
+        for (int i = 0; i < n; i++) output[i] = arr[i];
+        output[n] = insert;
+
+        // sort again
+        for (int i = 0; i < output.Length; i++)
+        {
+            for (int j = i + 1; j < output.Length; j++)
+            {
+                if (output[i] > output[j])
+                {
+                    int temp = output[i];
+                    output[i] = output[j];
+                    output[j] = temp;
+                }
+            }
+        }
+
+        Console.WriteLine(string.Join(", ", output));
     }
 }
