d304e22fed07c1d7927b1c0cdfdfb0fd737e02d0
Pawani541 <2005pawani@gmail.com>
2026-03-08 12:34:45 +0530

    Add files via upload

--- Week1--6_Jan_26to11_Jan_26/Day4_9_Jan_2026/Program.cs
+++ Week1--6_Jan_26to11_Jan_26/Day4_9_Jan_2026/Program.cs
@@ -1,55 +1,20 @@
 ﻿using System;
-
-class CompareArrays
+class FactProg
 {
     static void Main()
     {
-        Console.Write("Enter size of both arrays: ");
-        int size = int.Parse(Console.ReadLine());
+        int n = int.Parse(Console.ReadLine());
+        int output1 = 0;
 
-        int[] a = new int[size];
-        int[] b = new int[size];
-        int[] output = new int[size];
-
-        if (size < 0)
-        {
-            output[0] = -2;
-        }
+        if (n < 0) output1 = -1;
+        else if (n > 7) output1 = -2;
         else
         {
-            bool neg = false;
-
-            Console.WriteLine("Enter first array values:");
-            for (int i = 0; i < size; i++)
-            {
-                Console.Write("a[" + i + "] = ");
-                a[i] = int.Parse(Console.ReadLine());
-                if (a[i] < 0) neg = true;
-            }
-
-            Console.WriteLine("\nEnter second array values:");
-            for (int i = 0; i < size; i++)
-            {
-                Console.Write("b[" + i + "] = ");
-                b[i] = int.Parse(Console.ReadLine());
-                if (b[i] < 0) neg = true;
-            }
-
-            if (neg)
-            {
-                output[0] = -1;
-            }
-            else
-            {
-                for (int i = 0; i < size; i++)
-                {
-                    output[i] = Math.Max(a[i], b[i]);
-                }
-            }
+            int fact = 1;
+            for (int i = 1; i <= n; i++) fact *= i;
+            output1 = fact;
         }
-
-        Console.WriteLine("\nOutput array:");
-        foreach (int v in output)
-            Console.Write(v + " ");
+        Console.WriteLine(output1);
     }
 }
+
