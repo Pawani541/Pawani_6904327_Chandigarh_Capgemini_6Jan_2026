b0f45ff10fbd26f72536ea94def195a2c15e86fd
Pawani541 <2005pawani@gmail.com>
2026-03-08 13:20:03 +0530

    Add files via upload

---
+++ Week3--19_Jan_26to25_Jan_26/Day__2_20th_Jan_2026/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class LuckyString
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        string str = Console.ReadLine();
+
+        // Invalid case
+        if (n > str.Length)
+        {
+            Console.WriteLine("Invalid");
+            return;
+        }
+
+        int required = n / 2;
+
+        // Check all substrings of length n
+        for (int i = 0; i <= str.Length - n; i++)
+        {
+            string sub = str.Substring(i, n);
+
+            // Check condition 1: only P, S, G
+            bool validChars = true;
+            foreach (char c in sub)
+            {
+                if (c != 'P' && c != 'S' && c != 'G')
+                {
+                    validChars = false;
+                    break;
+                }
+            }
+
+            if (!validChars)
+                continue;
+
+            // Check condition 2: at least n/2 consecutive P or S or G
+            int count = 1;
+            for (int j = 1; j < sub.Length; j++)
+            {
+                if (sub[j] == sub[j - 1])
+                {
+                    count++;
+                    if (count >= required)
+                    {
+                        Console.WriteLine("Yes");
+                        return;
+                    }
+                }
+                else
+                {
+                    count = 1;
+                }
+            }
+        }
+
+        // If no lucky substring found
+        Console.WriteLine("No");
+    }
+}
