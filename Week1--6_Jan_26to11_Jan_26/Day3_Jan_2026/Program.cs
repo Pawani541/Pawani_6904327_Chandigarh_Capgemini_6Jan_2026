98c9362a897b7381015d29c87128f81bac1beb87
Pawani541 <2005pawani@gmail.com>
2026-03-08 12:26:22 +0530

    Add files via upload

---
+++ Week1--6_Jan_26to11_Jan_26/Day3_Jan_2026/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Press any key...");
+
+        ConsoleKeyInfo key = Console.ReadKey();
+        Console.WriteLine();
+
+        if (key.KeyChar >= '0' && key.KeyChar <= '9')
+            Console.WriteLine("You pressed number: " + key.KeyChar);
+        else
+            Console.WriteLine("Not allowed");
+    }
+}
+
