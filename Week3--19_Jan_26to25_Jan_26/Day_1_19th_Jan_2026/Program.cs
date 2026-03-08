5b49c61273a842fa6d4bc0304e45808367c8c600
Pawani541 <2005pawani@gmail.com>
2026-03-08 13:16:25 +0530

    Add files via upload

---
+++ Week3--19_Jan_26to25_Jan_26/Day_1_19th_Jan_2026/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+class BankAccount
+{
+    public int AccountNo 
+    {
+        get; 
+        private set;
+    }
+    public string HolderName
+    {
+        get;
+        private set;
+    }
+    protected double Balance;
+
+    public BankAccount(int accNo, string name, double balance)
+    {
+        AccountNo = accNo;
+        HolderName = name;
+        Balance = balance;
+    }
+
+    public void Deposit(double amount)
+    {
+        Balance += amount;
+    }
+
+    public void Withdraw(double amount)
+    {
+        if (amount <= Balance)
+            Balance -= amount;
+        else
+            Console.WriteLine("Insufficient balance");
+    }
+
+    public virtual void Display()
+    {
+        Console.WriteLine($"Account: {AccountNo}, Name: {HolderName}, Balance: {Balance}");
+    }
+}
+
+class SavingsAccount : BankAccount
+{
+    public SavingsAccount(int accNo, string name, double balance)
+        : base(accNo, name, balance) { }
+
+    public void AddInterest()
+    {
+        Balance += Balance * 0.04;
+    }
+}
+
+class CheckingAccount : BankAccount
+{
+    public CheckingAccount(int accNo, string name, double balance)
+        : base(accNo, name, balance) { }
+}
+
+class Program
+{
+    static void Main()
+    {
+        SavingsAccount sa = new SavingsAccount(101, "Aarav", 10000);
+        sa.AddInterest();
+        sa.Display();
+    }
+}
