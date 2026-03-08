dd013875edcb488df36556c3cf51f54c762cc91e
Pawani541 <2005pawani@gmail.com>
2026-03-08 13:10:09 +0530

    Add files via upload

--- Week2--12_Jan_26to18_Jan_26/Day4_15_Jan_2026/Program.cs
+++ Week2--12_Jan_26to18_Jan_26/Day4_15_Jan_2026/Program.cs
@@ -4,23 +4,34 @@ class Program
 {
     static void Main()
     {
-        Console.Write("Enter number: ");
+        Console.Write("Enter size: ");
         int n = Convert.ToInt32(Console.ReadLine());
 
-        if (n < 0) 
-        {
-            Console.WriteLine(-1); 
-            return;
-            }
-
-        int temp = n, rev = 0;
-        while (temp > 0)
-        {
-            rev = rev * 10 + temp % 10;
-            temp /= 10;
+        if (n < 0) { 
+            Console.WriteLine(-2); return; 
         }
 
-        if (rev == n) Console.WriteLine(1);
-        else Console.WriteLine(-2);
+        int[] arr = new int[n];
+        Console.WriteLine("Enter elements:");
+
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = Convert.ToInt32(Console.ReadLine());
+            if (arr[i] < 0) 
+            {
+                Console.WriteLine(-1); return; 
+            }
+        }
+
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < n; i++)
+        {
+            if (arr[i] < min) min = arr[i];
+            if (arr[i] > max) max = arr[i];
+        }
+
+        Console.WriteLine(max * min);
     }
 }
