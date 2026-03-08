1e0385968fd18e30a8e37ab51ea57a90d22b192f
Pawani541 <2005pawani@gmail.com>
2026-03-08 12:48:14 +0530

    Add files via upload

---
+++ Week1--6_Jan_26to11_Jan_26/Student Registration System/SeniorStudent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace StudentRegistrationSystem
+{
+    internal class SeniorStudent : Registration
+    {
+        public int PassingYear;
+        public string Responsibility;
+
+        public SeniorStudent(int admissionNo, int passingYear, string responsibility)
+            : base(admissionNo) 
+        {
+            PassingYear = passingYear;
+            Responsibility = responsibility;
+        }
+
+        public void DisplaySenior()
+        {
+            Display(); // parent class display
+            Console.WriteLine($"Passing Year : {PassingYear}");
+            Console.WriteLine($"Responsibility : {Responsibility}");
+        }
+    }
+}
---
+++ Week1--6_Jan_26to11_Jan_26/Student Registration System/StudentValidator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public class StudentValidator
+{
+    public void ValidateStudent(int studentId)
+    {
+        try
+        {
+            // Rule: Student ID positive   
+            if (studentId <= 0)
+            {
+                throw new Exception("Invalid Student ID");
+            }
+
+            Console.WriteLine("Student ID is VALID ");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Student ID is INVALID ");
+            Console.WriteLine("Reason: " + ex.Message);
+        }
+    }
+}
