c4acd7406d3b769efa09371a9ef7b142199be22a
Pawani541 <2005pawani@gmail.com>
2026-03-08 12:37:03 +0530

    Add files via upload

--- Week1--6_Jan_26to11_Jan_26/Day4_9_Jan_2026/Program.cs
+++ Week1--6_Jan_26to11_Jan_26/Day4_9_Jan_2026/Program.cs
@@ -1,44 +1,17 @@
 ﻿using System;
-class MultiplyArrays
+class CountDigits
 {
     static void Main()
     {
-        int size = int.Parse(Console.ReadLine());
-        int[] a = new int[size];
-        int[] b = new int[size];
-        int[] output = new int[size];
+        int n = int.Parse(Console.ReadLine());
+        int output1 = 0;
 
-        if (size < 0)
-        {
-            output[0] = -2;
-        }
+        if (n < 0) output1 = -1;
         else
         {
-            bool neg = false;
-
-            for (int i = 0; i < size; i++)
-            {
-                a[i] = int.Parse(Console.ReadLine());
-                if (a[i] < 0) neg = true;
-            }
-            for (int i = 0; i < size; i++)
-            {
-                b[i] = int.Parse(Console.ReadLine());
-                if (b[i] < 0) neg = true;
-            }
-
-            if (neg) output[0] = -1;
-            else
-            {
-                Array.Sort(a);
-                Array.Sort(b);
-                Array.Reverse(b);
-                for (int i = 0; i < size; i++)
-                {
-                    output[i] = a[i] * b[i];
-                }
-            }
+            output1 = n.ToString().Length;
         }
-        foreach (int v in output) Console.Write(v + " ");
+        Console.WriteLine(output1);
     }
 }
+
