ee02d0740f853be1b4b092fa340edf762f6f275a
Pawani541 <2005pawani@gmail.com>
2026-03-08 12:53:10 +0530

    Add files via upload

--- Week2--12_Jan_26to18_Jan_26/Day1_12_Jan_2026/Program.cs
+++ Week2--12_Jan_26to18_Jan_26/Day1_12_Jan_2026/Program.cs
@@ -1,28 +1,40 @@
 ﻿using System;
-
-class CubePrimeSum
+class SumAvgMult5
 {
-    static bool IsPrime(int n)
-    {
-        if (n < 2) return false;
-        for (int i = 2; i * i <= n; i++)
-            if (n % i == 0) return false;
-        return true;
-    }
-
     static void Main()
     {
-        Console.Write("Enter n: ");
-        int n = int.Parse(Console.ReadLine());
+        Console.Write("Enter size of array: ");
+        int size = int.Parse(Console.ReadLine());
         int output = 0;
 
-        if (n < 0) output = -1;
-        else if (n > 32676) output = -2;
+        if (size < 0) output = -2;
         else
         {
-            for (int i = 1; i <= n; i++)
-                if (IsPrime(i))
-                    output += i * i * i;
+            int[] arr = new int[size];
+            bool neg = false;
+            int sum = 0, count = 0;
+
+            Console.WriteLine("Enter elements:");
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write("arr[" + i + "] = ");
+                arr[i] = int.Parse(Console.ReadLine());
+                if (arr[i] < 0) neg = true;
+            }
+
+            if (neg) output = -1;
+            else
+            {
+                foreach (int v in arr)
+                {
+                    if (v % 5 == 0)
+                    {
+                        sum += v;
+                        count++;
+                    }
+                }
+                output = (count > 0) ? sum / count : 0;
+            }
         }
 
         Console.WriteLine("Output: " + output);
